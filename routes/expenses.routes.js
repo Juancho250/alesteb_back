6d2b7e6f999dca58e26690a0f46f63e0e897712e
Juancho250 <130582206+Juancho250@users.noreply.github.com>
2026-01-19 18:46:34 -0500

    listo

--- routes/expenses.routes.js
+++ routes/expenses.routes.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import {
   getExpenses,
-  createExpense,
-  getFinanceSummary
+  getFinanceSummary,
+  createExpense
 } from "../controllers/expenses.controller.js";
-import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", authMiddleware, getExpenses);
-router.post("/", authMiddleware, createExpense);
-router.get("/summary", authMiddleware, getFinanceSummary);
+router.get("/", getExpenses);
+router.get("/summary", getFinanceSummary);
+router.post("/", createExpense);
 
 export default router;
