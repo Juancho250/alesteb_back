741a28130783a14147b754d2b198de6a480df906
Juancho250 <130582206+Juancho250@users.noreply.github.com>
2026-01-19 18:54:59 -0500

    listo

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
-app.use("/expenses", expensesRoutes);
+app.use("/api/expenses", expensesRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/sales", salesRoutes);
