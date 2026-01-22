b9f8357d01d9e9ddc55fcbeca898bfb2c62c817d
Juancho250 <130582206+Juancho250@users.noreply.github.com>
2026-01-21 22:13:52 -0500

    ok

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ import helmet from "helmet";
 import expensesRoutes from "./routes/expenses.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/products.routes.js";
+import categoryRoutes from "./routes/categories.routes.js"; // ✅ Añadido
 import salesRoutes from "./routes/sales.routes.js";
 import usersRoutes from "./routes/users.routes.js";
 import rolesRoutes from "./routes/roles.routes.js";
 import bannerRoutes from "./routes/banners.routes.js";
-import discountRoutes from "./routes/discounts.routes.js"; // Añade esto
+import discountRoutes from "./routes/discounts.routes.js";
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(helmet());
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expensesRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes); // ✅ Registrado
 app.use("/api/discounts", discountRoutes);
 app.use("/api/sales", salesRoutes);
 app.use("/api/users", usersRoutes);
