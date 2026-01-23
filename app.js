a976eb1e947a6d57f23f26c11471f870bb7ff1ce
Juancho250 <130582206+Juancho250@users.noreply.github.com>
2026-01-23 18:41:19 -0500

    ok

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
+  const express = require("express");
+  const cors = require("cors");
+  const helmet = require("helmet");
 
-const expensesRoutes = require("./routes/expenses.routes");
-const authRoutes = require("./routes/auth.routes");
-const productRoutes = require("./routes/products.routes");
-const categoryRoutes = require("./routes/categories.routes");
-const salesRoutes = require("./routes/sales.routes");
-const usersRoutes = require("./routes/users.routes");
-const rolesRoutes = require("./routes/roles.routes");
-const bannerRoutes = require("./routes/banners.routes");
-const discountRoutes = require("./routes/discounts.routes");
-const permissions = require("./routes/permissions.routes");
+  const expensesRoutes = require("./routes/expenses.routes");
+  const authRoutes = require("./routes/auth.routes");
+  const productRoutes = require("./routes/products.routes");
+  const categoryRoutes = require("./routes/categories.routes");
+  const salesRoutes = require("./routes/sales.routes");
+  const usersRoutes = require("./routes/users.routes");
+  const rolesRoutes = require("./routes/roles.routes");
+  const bannerRoutes = require("./routes/banners.routes");
+  const discountRoutes = require("./routes/discounts.routes");
+  const permissions = require("./routes/permissions.routes");
 
-const app = express();
+  const app = express();
 
-app.use(cors({
-  origin: "*",
-  allowedHeaders: ["Content-Type", "Authorization"],
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+  app.use(cors({
+    origin: "*",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }));
 
-app.use(express.json());
-app.use(helmet());
+  app.use(express.json());
+  app.use(helmet());
 
-// RUTAS
-app.use("/api/auth", authRoutes);
-app.use("/api/expenses", expensesRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/categories", categoryRoutes);
-app.use("/api/discounts", discountRoutes);
-app.use("/api/sales", salesRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/roles", rolesRoutes);
-app.use("/api/banners", bannerRoutes);
-app.use("/api/permissions", permissions);
+  // RUTAS
+  app.use("/api/auth", authRoutes);
+  app.use("/api/expenses", expensesRoutes);
+  app.use("/api/products", productRoutes);
+  app.use("/api/categories", categoryRoutes);
+  app.use("/api/discounts", discountRoutes);
+  app.use("/api/sales", salesRoutes);
+  app.use("/api/users", usersRoutes);
+  app.use("/api/roles", rolesRoutes);
+  app.use("/api/banners", bannerRoutes);
+  app.use("/api/permissions", permissions);
 
-app.get("/", (_, res) => {
-  res.send("API Alesteb OK");
-});
+  app.get("/", (_, res) => {
+    res.send("API Alesteb OK");
+  });
 
-module.exports = app;
+  module.exports = app;
