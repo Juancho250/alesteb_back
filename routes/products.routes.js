3a04e1d44f71e353bd2d94eeb1684ef17a2c1d39
Juancho250 <130582206+Juancho250@users.noreply.github.com>
2026-01-28 21:33:03 -0500

    ok

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,35 +1,27 @@
 const express = require("express");
-const { auth, requireRole } = require("../middleware/auth.middleware");
 const upload = require("../middleware/upload.middleware");
 const ctrl = require("../controllers/products.controller");
 
 const router = express.Router();
 
-// 🌐 PÚBLICO
+// Todas las rutas públicas
 router.get("/", ctrl.getAll);
 router.get("/:id", ctrl.getById);
 
-// 🔐 SOLO ADMIN
 router.post(
   "/",
-  auth,
-  requireRole(["admin"]),
   upload.array("images", 6),
   ctrl.create
 );
 
 router.put(
   "/:id",
-  auth,
-  requireRole(["admin"]),
-  upload.array("images", 6), // <--- AGREGAR ESTO AQUÍ
+  upload.array("images", 6),
   ctrl.update
 );
 
 router.delete(
   "/:id",
-  auth,
-  requireRole(["admin"]),
   ctrl.remove
 );
 
