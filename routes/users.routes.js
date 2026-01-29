ab9c7f7542f8058a39d44b1b7ddd058f2176d802
Juancho250 <130582206+Juancho250@users.noreply.github.com>
2026-01-28 21:41:33 -0500

    ok

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/users.controller'); 
-const { auth, isAdmin } = require('../middleware/auth.middleware');
+const usersController = require('../controllers/users.controller');
 
-router.get('/', auth, isAdmin, usersController.getUsers);
-router.post('/', auth, isAdmin, usersController.createUser);
-router.put('/:id', auth, isAdmin, usersController.updateUser);
-router.delete('/:id', auth, isAdmin, usersController.deleteUser); // Aquí fallaba
+router.get('/', usersController.getUsers);
+router.post('/', usersController.createUser);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
